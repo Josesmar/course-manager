b6db3104a8614b6672c87a14d41a3bcb4cb9d260
Josesmar <josesmargds@gmail.com>
2021-07-10 17:25:46 -0300

    criando a pontuação de cada curso

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,36 @@
 import { Template } from "@angular/compiler/src/render3/r3_ast";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
     selector: 'app-course-list', //Vai virar uma tag e ser usando em outro compomente
     templateUrl: './course-list.component.html'
 })
-export class CourseListComponent{
-    //Fazer referência aqui
+export class CourseListComponent implements OnInit{
+    courses: Course[] = [];
+
+    ngOnInit(): void{ //ao utilizar o implements Oninit é obrigatório a criação desse Método ngOnInit
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '/assets/images/forms.png',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'julho. 10, 2021'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '/assets/images/http.png',
+                price: 45.99,
+                code: 'LKL-8796',
+                duration: 80,
+                rating: 4.2,
+                releaseDate: 'julho, 10,2021'
+            }
+        ]
+    }
 }
