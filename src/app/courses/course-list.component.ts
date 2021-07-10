99e24f9f1f491864d0c9dd880ad1e7159adf7968
Josesmar <josesmargds@gmail.com>
2021-07-10 17:38:27 -0300

    criando serviço  dos cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,29 @@
 import { Template } from "@angular/compiler/src/render3/r3_ast";
+<<<<<<< Updated upstream
 import { Component } from "@angular/core";
+=======
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+import { CourseService } from "./course-service";
+>>>>>>> Stashed changes
 
 @Component({
     selector: 'app-course-list', //Vai virar uma tag e ser usando em outro compomente
     templateUrl: './course-list.component.html'
 })
+<<<<<<< Updated upstream
 export class CourseListComponent{
     //Fazer referência aqui
+=======
+export class CourseListComponent implements OnInit{
+    courses: Course[] = [];
+
+    //Gerando uma instância com os mesmo objeto
+    constructor(private courseService: CourseService){}
+
+    ngOnInit(): void{ //ao utilizar o implements Oninit é obrigatório a criação desse Método ngOnInit
+        this.courses = this.courseService.retrieveAll();
+
+    }
+>>>>>>> Stashed changes
 }
--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 export class Course{
     id: number;
     name: String;
@@ -6,4 +7,16 @@ export class Course{
     code: string;
     duration: number;
     rating: number;
+=======
+export class Course {
+    id: number =0;
+    name: String ="";
+    imageUrl: string ="";
+    price: number =0;
+    code: string ="";
+    duration: number =0;
+    rating: number = 0;
+    releaseDate: string ="";
+    description: string="";
+>>>>>>> Stashed changes
 }
